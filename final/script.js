3c5d9bd829d5d758a12977a6fa54f3c11f13c9ec
michellu0929 <sunp952@newschool.edu>
2015-05-04 00:35:42 -0400

    hcshd

--- final/script.js
+++ final/script.js
@@ -122,11 +122,27 @@ $(function(){
 
     $.getJSON('image/image.json', function(data) {
         $.each(data.images, function(i,item) {
-            $("#imgrid ul").append("<li><a href='"+item.link+"'> <img src='"+item.src+"'/></a></li><br />");
+            $("#imgrid ul").append("<li><a href='"+item.link+"'> <img src='"+item.src+"'/></a></li><br/>");
 
         });
     });
 
 
 
+
+function showImage(imgName) {
+    document.getElementById('largeImg').src = imgName;
+    showLargeImagePanel();
+    unselectAll();
+}
+function showLargeImagePanel() {
+    document.getElementById('largeImgPanel').style.visibility = 'visible';
+}
+function unselectAll() {
+    if(document.selection) document.selection.empty();
+    if(window.getSelection) window.getSelection().removeAllRanges();
+}
+function hideMe(obj) {
+    obj.style.visibility = 'hidden';
+}
 
