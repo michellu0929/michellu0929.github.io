c750083568bf104d586fac201736b6bee87ca981
SunPei Yu <michelluSUN@SunPeis-MBP.fios-router.home>
2017-08-02 15:32:34 -0400

    hihi

--- main.js
+++ main.js
@@ -62,4 +62,9 @@ $(document).ready(function () {
 });
 
 
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
 
